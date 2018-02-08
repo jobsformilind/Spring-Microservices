04038c461d314e02e20cecf823e5b8c8713022aa
Milind Patil <Milind.Patil@siriusxm.com>
2018-02-07 21:40:28 -0500

    Commiting changes

---
+++ 01-micro-services-basic-config/src/main/java/com/test/spring/boot/restservices/user/UserController.java
@@ -0,0 +1,32 @@
+package com.test.spring.boot.restservices.user;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class ConfigurationController {
+
+	@GetMapping(path = "/config")
+	public  getConfiguration() {
+		
+	}
+}
