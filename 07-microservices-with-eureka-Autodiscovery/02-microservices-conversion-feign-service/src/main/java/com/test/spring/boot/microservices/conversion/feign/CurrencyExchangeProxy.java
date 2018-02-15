e71b639eedbfbc0982d0b47d84595fdf66403ec2
Mil Pat <milindain@yahoo.com>
2018-02-14 21:26:18 -0500

    Commiting changes

--- 07-microservices-with-eureka-Autodiscovery/02-microservices-conversion-feign-service/src/main/java/com/test/spring/boot/microservices/conversion/feign/CurrencyExchangeProxy.java
+++ 07-microservices-with-eureka-Autodiscovery/02-microservices-conversion-feign-service/src/main/java/com/test/spring/boot/microservices/conversion/feign/CurrencyExchangeProxy.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.test.spring.boot.microservices.conversion.service.ConversionValue;
 
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
-	@GetMapping("/currency-exchange/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/{from}/to/{to}")
+	@GetMapping("/currency-conversion-service/currency-exchange/{from}/to/{to}")
 	public ConversionValue retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
 }
