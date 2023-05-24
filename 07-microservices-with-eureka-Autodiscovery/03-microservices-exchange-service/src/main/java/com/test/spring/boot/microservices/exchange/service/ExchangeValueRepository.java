c6fc74477cabf1bc339c452840aeebe798e7870d
Milind <jobs.for.milind@gmail.com>
2023-05-24 23:48:36 +0530

    Commiting changes

--- 07-microservices-with-eureka-Autodiscovery/03-microservices-exchange-service/src/main/java/com/test/spring/boot/microservices/exchange/service/ExchangeValueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.spring.boot.microservices.exchange.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Integer> {
-	ExchangeValue findByFromAndTo(String from, String to);
-}
